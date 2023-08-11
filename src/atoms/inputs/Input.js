d703ccd9469c436c4d849e9db25c3a69fcc7e095
AJ Rugen <ARugen@boonecountymo.org>
2023-08-11 15:48:46 -0500

    worked on input controls

--- src/atoms/inputs/Input.js
+++ src/atoms/inputs/Input.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { placeholder, types } from '../../utils/constants';
 //Placing type before props spread will default to type and allow props
 //to override type, inverse will not override type
 export const Input = ({ ...props }) => {
-  return <input type="text" {...props} />;
+    function handleInputKey(e) {
+        const key = e.keyCode === 32 ? 'Space' : e.key
+        const type = e.target.type
+        //const maxLength = e.target.maxLength
+        //const length = e.target.value.length
+        //console.log(maxLength,length)
+        if(!(types[type].keys.test(key)) && !['Delete','Backspace','ArrowLeft','ArrowRight'].includes(key)) e.preventDefault();
+    }
+  return <input type="text" maxLength={3} placeholder={placeholder[props.type]} {...props} onKeyDown={handleInputKey}/>;
 };
 Input.displayName = 'Input';
