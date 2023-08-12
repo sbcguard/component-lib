ac6b2a08f1e8f2015179c94ea4b5aa62371d0ebf
AJ Rugen <arugen@boonecountymo.org>
2023-08-12 09:55:19 -0500

    'autosave'

--- src/atoms/inputs/Input.js
+++ src/atoms/inputs/Input.js
@@ -4,31 +4,48 @@ import './Input.css';
 //Placing type before props spread will default to type and allow props
 //to override type, inverse will not override type
 export const Input = ({ ...props }) => {
-  const [ errMsg, setErrMsg ] = useState('')
-  const [ type, ] = useState(props.type)
-  const [ keys, ] = useState(types[type].keys)
-  const [ error, ] = useState(types[type].error)
-  const [ checkVal, ] = useState(types[type].value)
+  const [errMsg, setErrMsg] = useState('');
+  const [type] = useState(props.type);
+  const [keys] = useState(types[type].keys);
+  const [error] = useState(types[type].error);
+  const [checkVal] = useState(types[type].value);
   const handleInputKey = (e) => {
-      const { key } = e
-      const resolvedKey = key === ' ' ? 'Space' : key
-      if(!(keys.test(resolvedKey)) && !['Delete','Backspace','ArrowLeft','ArrowRight'].includes(key)) e.preventDefault();
-  }
-  const handleChange = (e) => {
-    const { target } = e
-    const { required } = target
-    let { value } = target
-    const isNull = value.trim().length === 0
-    required && isNull && setErrMsg(error)
-    required && !isNull && !checkVal.test(value) && setErrMsg(error)
-    !required && !isNull && !checkVal.test(value) && setErrMsg(error)
-    required && !isNull && checkVal.test(value) && setErrMsg('')
-    !required && !isNull && checkVal.test(value) && setErrMsg('')
-    !required && isNull && !value && setErrMsg('')
-  }
-  return <div className='input-container'>
-            <input type="text" maxLength={ 3 } placeholder={ placeholder[props.type] } {...props} onChange={handleChange} onKeyDown={handleInputKey} />
-            <span className='error'>{ errMsg }</span>
-          </div>;
+    const { key } = e;
+    const resolvedKey = key === ' ' ? 'Space' : key;
+    if (
+      !keys.test(resolvedKey) &&
+      !['Delete', 'Backspace', 'ArrowLeft', 'ArrowRight'].includes(key)
+    )
+      e.preventDefault();
+  };
+  const handleInput = (e) => {
+    const { target } = e;
+    const { required } = target;
+    let { value } = target;
+    const isNull = value.trim().length === 0;
+    required && isNull && setErrMsg(error);
+    required && !isNull && !checkVal.test(value) && setErrMsg(error);
+    !required && !isNull && !checkVal.test(value) && setErrMsg(error);
+    required && !isNull && checkVal.test(value) && setErrMsg('');
+    !required && !isNull && checkVal.test(value) && setErrMsg('');
+    !required && isNull && !value && setErrMsg('');
+  };
+  return (
+    <div className="input-container">
+      <input
+        type="text"
+        maxLength={3}
+        placeholder={placeholder[props.type]}
+        /* Attributes above overridden by pass props */
+        {...props}
+        /* Attributes below preserved */
+        onInput={handleInput}
+        onChange={handleInput}
+        onBlur={handleInput}
+        onKeyDown={handleInputKey}
+      />
+      <span className="error">{errMsg}</span>
+    </div>
+  );
 };
 Input.displayName = 'Input';
--- src/stories/Inputs/Input.stories.js
+++ src/stories/Inputs/Input.stories.js
@@ -3,31 +3,40 @@
 import { Input } from '../../atoms/inputs';
 import { types } from '../../utils/constants';
 //Commented code is for use with Typescript
-const meta/*: Meta<typeof Input>*/ = {
+const meta /*: Meta<typeof Input>*/ = {
   title: 'Inputs/Input',
   component: Input,
   argTypes: {
     type: {
-      options: Object.keys(types).filter(opt => !['file','select-one','select-multiple','radio','checkbox'].includes(opt)),
+      options: Object.keys(types).filter(
+        (opt) =>
+          ![
+            'file',
+            'select-one',
+            'select-multiple',
+            'radio',
+            'checkbox',
+          ].includes(opt),
+      ),
       description: 'Input type attribute',
-      control: { type: 'select' }
+      control: { type: 'select' },
     },
     maxLength: {
       description: 'Input value maximum length',
-      control: { type: 'number'}
+      control: { type: 'number' },
     },
     required: {
       description: 'Require not NULL for Submission',
-      control: { type: 'boolean' }
-    }
+      control: { type: 'boolean' },
+    },
   },
 };
 export default meta;
 //type Story = StoryObj<typeof Input>;
-const Template = args => <Input {...args} />;
-export const Default = Template.bind({})
+const Template = (args) => <Input {...args} />;
+export const Default = Template.bind({});
 Default.args = {
   type: 'number',
   required: false,
-  maxLength: 15
-} 
+  maxLength: 15,
+};
