f59e32b0eb2cf3702e2922b7b500cc1e7e235346
AJ Rugen <ARugen@boonecountymo.org>
2023-08-24 16:46:30 -0500

    'autosave'

---
+++ src/stories/Inputs/Checkbox.stories.ts
@@ -0,0 +1,45 @@
+/* eslint-disable react/react-in-jsx-scope */
+import type { Meta, StoryObj } from '@storybook/react';
+import { Checkbox } from '../../components/atoms/inputs/checkbox';
+//Commented code is for use with Typescript
+/** Radios submitted their value when selected. Typically used in a group of radios where at lease 1 selection is required. <br />*/
+/** The constructor consist of 3 properties/arguements: <br />*/
+/** name - The name of the table field associated with this input. <br />*/
+/** value - Value submitted with the field when radio is selected/checked. <br />*/
+/** children (label) - Displayed value of the radio label. <br />*/
+const meta = {
+  title: 'Form Elements/Inputs/Radio',
+  component: Checkbox,
+  tags: ['autodocs'],
+  argTypes: {
+    type: {
+      control: { disable: true },
+    },
+    name: {
+      description: 'Field name associated with the radio.',
+      control: { type: 'text' },
+    },
+    value: {
+      description: 'Value submitted by a checked radio.',
+      control: { type: 'text' },
+    },
+    children: {
+      description: 'Displayed label value of the radio.',
+      control: { type: 'text' },
+    },
+  },
+} satisfies Meta<typeof Checkbox>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Single: Story = {
+  args: {
+    type: 'checkbox',
+    name: 'Field Name',
+    checked: true,
+    value: 'TEST',
+    children: 'TEST',
+  },
+};
